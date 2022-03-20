b95a098745c2b7c55c76af485c35db103d2fc5a8
LevKholopov <95305388+LevKholopov@users.noreply.github.com>
2022-03-20 20:17:11 +0200

    16:47.

--- EMS_0.2_Server/Router.cs
+++ EMS_0.2_Server/Router.cs
@@ -14,6 +14,9 @@ namespace EMS_Server
             {
                 default: { throw new Exception($"Requested action was not found! Check DataPacket._header.Act!\n_header={data._header}\nAct={data._header.Act}"); }
                 /*Select employee*/case 1: { return SQLBridge.TwoWayCommand(SQLBridge.Select(data.StringData)); }
+                /*Add employee*/   case 2: { return SQLBridge.OneWayCommand(SQLBridge.Add(data.StringData)); }
+                /*Update employee*/case 3: { return SQLBridge.OneWayCommand(SQLBridge.Update(data.StringData)); }
+                /*Delete employee*/case 4: { return SQLBridge.OneWayCommand(SQLBridge.Delete(data.StringData)); }
                 /*Direct querry*/  case 253: { return SQLBridge.OneWayCommand(data.StringData); }
                 /*Direct querry*/  case 254: { return SQLBridge.TwoWayCommand(data.StringData); }
                 /*Ping*/           case 255: { return data.StringData; }
